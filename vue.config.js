6ec997cf6b51cb8234dc84114978a04d811d2f44
yangsun <erfgh2948@naver.com>
2023-02-24 20:19:46 +0900

    .

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
-
 module.exports ={
   devServer:{
-    proxy:'https://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app'
+    proxy:{
+      '/api':{// /api라는 주소로 요청하면 밑으로 우회한다
+        target:"http://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app/",
+        changeOrigin:true,
+        pathRewrite:{
+          '^/':''
+        }
+      }
+    }
   }
 }
 
@@ -9,18 +16,9 @@ module.exports ={
 
 // module.exports ={
 //   devServer:{
-//     proxy:{
-//       '/api':{// /api라는 주소로 요청하면 밑으로 우회한다
-//         target:'https://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app/',
-//         changeOrigin:true,
-//         pathRewrite:{
-//           '^/':''
-//         }
-//       }
-//     }
+//     proxy:'https://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app'
 //   }
 // }
-
 
 
 // java:8080
