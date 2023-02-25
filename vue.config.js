3ed5b4e806312d688e2654913e7c3fd0ec3d6028
yangsun <erfgh2948@naver.com>
2023-02-25 20:06:05 +0900

    .

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports ={
   devServer:{
     proxy:{
       '/api':{// /api라는 주소로 요청하면 밑으로 우회한다
-        target:"http://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app/",
+        target:"https://port-0-wine-backend-4uvg2mledushse.sel3.cloudtype.app/",
         changeOrigin:true,
         pathRewrite:{
           '^/':''
